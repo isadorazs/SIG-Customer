7fdfb42b0cda6d1d6935bf9627fe75317cecec8d
rafa-21 <71237443+rafa-21@users.noreply.github.com>
2021-01-31 01:43:16 -0300

    Adicionando novas funções

---
+++ assinaturas.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuPrincipal(void);
+void menuCliente(void);
+void menuVendas(void);
+void menuPagamento(void);
+void menuEstoque(void);
+void telaCadastrarCliente(void);
+void telaCadVenda(void);
+void telaCadPag(void);
+void telaRegProd(void);
+void telaBuscarCliente(void);
+void telaExcCliente(void);
+void telaEditCliente(void);
+void telaExcProd(void);
